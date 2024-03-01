7a419a9a9c2772aa15f632e4777d7c4a55242c91
etim david <etimdavid407@gmail.com>
2024-03-01 13:13:28 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Slidshow from './component/Slidshow'
+import Footer from './component/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Slidshow/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
